defaa4ff19c0ddb8345106579a55c423cd147c4f
yungsan <jjzaczac@gmail.com>
2022-07-22 08:35:51 +0700

    fix bug

--- routers/foodsRouter.js
+++ routers/foodsRouter.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const upload = require('../middleware/upload');
+const upload = require("../middleware/upload");
 const foodsController = require("../controllers/foodsController");
+const cartsController = require("../controllers/cartsController");
 
 router
-.route("/addFood")
-.get(foodsController.addFood)
-.get((req, res) => res.json('add foods'))
-.post(upload.single('thumbnail'), foodsController._addFood);
+  .route("/addFood")
+  .get(foodsController.addFood)
+  .post(upload.single("thumbnail"), foodsController._addFood);
 
-router.get('/:id', foodsController.detail);
+router
+  .route("/:id")
+  .get(foodsController.detail)
+  .post(cartsController.addToCart);
 
 router.route("/").get(foodsController.index);
 
